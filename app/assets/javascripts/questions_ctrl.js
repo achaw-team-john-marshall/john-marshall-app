37b441160dfd8925009b59470b90b37f39fe5047
chloebethcanon <chloebethcanon@gmail.com>
2016-02-01 21:34:53 -0600

    added bracket to angular setup to show in heroku

--- app/assets/javascripts/questions_ctrl.js
+++ app/assets/javascripts/questions_ctrl.js
@@ -1,18 +1,18 @@
 /* global angular, $scope */
 
 (function() {
-  angular.module("app").controller("questionsCtrl", function($scope) {
+  angular.module("app").controller("questionsCtrl", ['$scope', function($scope) {
     $scope.setup = function() {
       $scope.question = 'question01';
     };
  
 
 
-  $scope.changeQuestion = function(question) {
-    $scope.question = question;
-    console.log(question);
-  };
+    $scope.changeQuestion = function(question) {
+      $scope.question = question;
+      console.log(question);
+    };
 
     window.$scope = $scope;
-  });
+  }]);
 })();
